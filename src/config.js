b9b34b14fbedf2493e19c5959f36c0d46e6a9efe
Dan Esparza <esparza.dan@gmail.com>
2017-06-26 20:46:22 -0400

    Add client websocket functionality

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const appconfig = {
   "serviceBaseUrl" : "http://cerdo.lan:3030" /* Leave blank for localhost/local service port */,
+  "websocketBaseUrl": "cerdo.lan:3030"
 };
 
 export default appconfig;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+//	App config
+import appconfig from './config'
+
 //	The API utils
 import APIutils from './utils/APIutils';
+
+//  Call API util methods:
 APIutils.getCurrentState();
 APIutils.getActivities();
 APIutils.getAllConfigItems();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+//	Listen to the websocket:
+let wsprotocol = "ws:";
+if(window.location.protocol === "https:"){ wsprotocol = "wss:"; }
+let ws = new WebSocket(`${wsprotocol}//${appconfig.websocketBaseUrl}/ws`)
+ws.addEventListener("message", function(e){     
+    //  Get the latest version of activities and state:
+    APIutils.getCurrentState();    
+    APIutils.getActivities();
+})
