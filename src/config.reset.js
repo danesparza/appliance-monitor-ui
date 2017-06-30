4b570f1c4bb803f6d01de1bed3884be6c9908da1
Dan Esparza <esparza.dan@gmail.com>
2017-06-30 11:08:54 -0400

    CI: reset config when building

---
+++ src/config.reset.js
@@ -0,0 +1,6 @@
+const appconfig = {
+  /* Specify service base host & port like this: cerdo.lan:3030 */
+  "serviceBaseHostPort" : "" /* Leave blank for localhost/local service port. */
+};
+
+export default appconfig;
